baabdfb92f015213e44c7c83daf249b29522d168
Brad Schrag <brad.schrag@gmail.com>
2018-11-07 18:32:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,12 +17,7 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.each do | call |
-    if call.length > 4
-      return true
-    end
-  end
-  return false
+  calls.find_all {|word| word.length > 4}
 end
 
 def find_the_cheese(snacks)# code an argument here
